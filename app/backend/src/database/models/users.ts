6eb73820af4a194d983f94677aff11e29a461a8f
Mariana Saraiva <marianascm8@gmail.com>
2022-05-30 21:29:13 -0300

    feat: create migrations, model from table users

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class users extends Model {
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+users.init({
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default users;
