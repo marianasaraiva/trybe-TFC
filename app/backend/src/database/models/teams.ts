334c0e802538ce8422dadee8f46e15d596cff98b
Mariana Saraiva <marianascm8@gmail.com>
2022-05-30 21:30:22 -0300

    feat: create migrations, model from table teams

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class teams extends Model {
+  teamName: string;
+}
+teams.init({
+  teamName: DataTypes.STRING,
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default teams;
