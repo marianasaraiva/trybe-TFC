a89cae90cd1cf0c29002ea7eb629a78bbd78ee49
Mariana Saraiva <marianascm8@gmail.com>
2022-05-31 21:35:42 -0300

    feat: coment references

--- app/backend/src/database/migrations/20220530232423-create-matches.js
+++ app/backend/src/database/migrations/20220530232423-create-matches.js
@@ -10,20 +10,20 @@ module.exports = {
       },
       home_team: {
         type: Sequelize.INTEGER,
-        references: {
-          model: "teams",
-          key: "id",
-        }
+        // references: {
+        //   model: "teams",
+        //   key: "id",
+        // }
       },
       home_team_goals: {
         type: Sequelize.INTEGER
       },
       away_team: {
         type: Sequelize.INTEGER,
-        references: {
-          model: "teams",
-          key: "id",
-        }
+        // references: {
+        //   model: "teams",
+        //   key: "id",
+        // }
       },
       away_team_goals: {
         type: Sequelize.INTEGER
