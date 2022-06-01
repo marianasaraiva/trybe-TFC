c3ddcbb100746bc4690baa8d0432d87fd2d8521d
Mariana Saraiva <marianascm8@gmail.com>
2022-05-31 21:40:57 -0300

    feat: create login services

---
+++ app/backend/src/database/services/login.ts
@@ -0,0 +1,29 @@
+import users from '../models/users';
+import { IUser } from '../interfaces/users';
+import generateToken from '../helpers/generateToken';
+import crypto from '../helpers/passwordCrypt';
+
+class LoginService {
+  constructor(private models = users) {}
+
+  login = async (email: string, password: string): Promise<IUser | null> => {
+    const user = await users.findOne({ where: { email } });
+    if (!user) return null;
+
+    const teste = crypto(password, user.password);
+    if (!teste) return null;
+
+    const token = generateToken({ data: { role: user.role, id: user.id } });
+
+    return {
+      user: {
+        id: user.id,
+        username: user.username,
+        role: user.role,
+        email: user.email,
+      },
+      token,
+    };
+  };
+}
+export default new LoginService();
