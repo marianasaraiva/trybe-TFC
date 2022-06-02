3db5a2ae752acc04be2357138f999964a0461315
Mariana Saraiva <marianascm8@gmail.com>
2022-06-02 12:08:09 -0300

    feat: add router post /matches

--- app/backend/src/database/routes/matches.ts
+++ app/backend/src/database/routes/matches.ts
@@ -5,5 +5,7 @@ import matchersController from '../controllers/matches';
 const matchRouter = express.Router();
 
 matchRouter.get('/', matchersController.getByQuery);
+matchRouter.post('/', matchersController.createMatch);
+matchRouter.patch('/:id/finish', matchersController.patchMatch);
 
 export default matchRouter;
