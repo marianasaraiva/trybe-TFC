cb408425d33eaee7ffe9e1e2dafaa9d0d5dfc70b
Mariana Saraiva <marianascm8@gmail.com>
2022-06-02 12:07:09 -0300

    feat: add createMatch and patchMatch in matches services

--- app/backend/src/database/services/matches.ts
+++ app/backend/src/database/services/matches.ts
@@ -1,6 +1,7 @@
 import matches from '../models/matches';
 import Teams from '../models/teams';
-import IMatch from '../interfaces/matchers';
+import { ICreateMatch, IMatch } from '../interfaces/matchers';
+import LoginService from './login';
 
 class MatchServices {
   constructor(private models = matches) {}
@@ -43,6 +44,25 @@ class MatchServices {
         }],
     });
     return game;
+  }
+
+  async createMatch(createUser: ICreateMatch, token: string): Promise<ICreateMatch | null> {
+    const verify = await LoginService.validateLogin(token);
+
+    if (!verify) return null;
+
+    const { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = createUser;
+    const newGame = await this.models
+      .create({ homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress });
+    return newGame;
+  }
+
+  async patchMatch(id: number): Promise<boolean> {
+    const patchByPk = await this.models.findByPk(id);
+    if (!patchByPk) return false;
+
+    await this.models.update({ inProgress: false }, { where: { id } });
+    return true;
   }
 }
 
